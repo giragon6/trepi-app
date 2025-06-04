abcd0bc912e8754ecd64ea2c03aef580df1b4c05
giragon6 <emmemcdonald66@gmail.com>
2025-06-04 16:20:10 -0400

    chore: deployment fixes

--- lib/core/config/app_config.dart
+++ lib/core/config/app_config.dart
@@ -1,7 +1,7 @@
 class AppConfig {
   static const String apiBaseUrl = String.fromEnvironment(
     'API_BASE_URL',
-    defaultValue: 'http://localhost:8000/api/v1',
+    defaultValue: 'https://trepi-api.giragon6.hackclub.app/api/v1',
   );
   
   static const bool isProduction = bool.fromEnvironment('PRODUCTION');
